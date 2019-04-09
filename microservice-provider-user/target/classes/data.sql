781b0e4d5d1e8ac878540b3cb02ddef5b1cb88e8
emperWang <544094478@qq.com>
2019-04-09 17:24:06 +0800

    remove target

--- microservice-provider-user/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user(id, username, name, age, balance) VALUES (1,'zhangsan','zs',10,100.00);
-INSERT INTO user(id, username, name, age, balance) VALUES (2,'wangwu','ww',10,100.00);
-INSERT INTO user(id, username, name, age, balance) VALUES (3,'zhaoliu','zl',10,100.00);
-INSERT INTO user(id, username, name, age, balance) VALUES (4,'qianqi','qq',10,100.00);
-INSERT INTO user(id, username, name, age, balance) VALUES (5,'sunba','sb',10,100.00);
