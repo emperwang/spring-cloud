6d2bcc4a39202ee206d850303ea1ed14838f66b2
emperWang <544094478@qq.com>
2019-04-09 10:27:09 +0800

    fix simple-provider application.yml and add repository annotation

--- microservice-simple-provider-user/src/main/java/com/wk/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
+++ microservice-simple-provider-user/src/main/java/com/wk/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -2,6 +2,7 @@ package com.wk.cloud.microservicesimpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class MicroserviceSimpleProviderUserApplication {
