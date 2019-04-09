be583147dbb97b08b33609cf6f1581abb50a689e
emperWang <544094478@qq.com>
2019-04-09 17:26:47 +0800

    basic ribbon in consumer move

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/wk/cloud/microservicesimpleconsumermovie/MicroserviceConsumerMovieRibbonApplication.java
@@ -0,0 +1,15 @@
+package com.wk.cloud.microservicesimpleconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroserviceConsumerMovieRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConsumerMovieRibbonApplication.class, args);
+	}
+
+}
