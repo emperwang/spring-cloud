ef4df361a784c7880e0cfc387df8f81b2aa46450
emperWang <544094478@qq.com>
2019-04-12 15:44:38 +0800

    open the function hystrix.stream in feign with hystrix

--- microservice-consumer-movie-feign-with-hystrix/src/main/java/com/wk/cloud/microservicesimpleconsumermovie/MicroserviceConsumerFeignHystrixMovieApplication.java
+++ microservice-consumer-movie-feign-with-hystrix/src/main/java/com/wk/cloud/microservicesimpleconsumermovie/MicroserviceConsumerFeignHystrixMovieApplication.java
@@ -2,12 +2,14 @@ package com.wk.cloud.microservicesimpleconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker   //此注解打开hystrix.stream监控
 public class MicroserviceConsumerFeignHystrixMovieApplication {
 
 	public static void main(String[] args) {
