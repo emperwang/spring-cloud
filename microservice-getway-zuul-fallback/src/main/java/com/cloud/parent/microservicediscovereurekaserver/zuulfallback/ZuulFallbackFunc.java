930aa17d346e98aac56d0785696495a88615acf7
emperWang <544094478@qq.com>
2019-04-13 15:42:20 +0800

    zuul fallback

---
+++ microservice-getway-zuul-fallback/src/main/java/com/cloud/parent/microservicediscovereurekaserver/zuulfallback/ZuulFallbackFunc.java
@@ -0,0 +1,65 @@
+package com.cloud.parent.microservicediscovereurekaserver.zuulfallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * zuul的断路器实现
+ */
+@Component
+public class ZuulFallbackFunc  implements ZuulFallbackProvider{
+
+    /**
+     * getRoute 返回的字符串，表示的是服务的名字
+     * @return
+     */
+    @Override
+    public String getRoute() {
+        return "microservice-provide-user";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("fallback".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
