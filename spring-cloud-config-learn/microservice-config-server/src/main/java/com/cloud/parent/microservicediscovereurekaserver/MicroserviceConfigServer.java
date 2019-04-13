18f52d16beea32b7bb8d6a32d6185cb19f959489
emperWang <544094478@qq.com>
2019-04-13 17:45:55 +0800

    basic config server

--- spring-cloud-config-learn/microservice-config-server/src/main/java/com/cloud/parent/microservicediscovereurekaserver/MicroserviceConfigServer.java
+++ spring-cloud-config-learn/microservice-config-server/src/main/java/com/cloud/parent/microservicediscovereurekaserver/MicroserviceConfigServer.java
@@ -2,9 +2,10 @@ package com.cloud.parent.microservicediscovereurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MicroserviceConfigServer {
 
 	public static void main(String[] args) {
