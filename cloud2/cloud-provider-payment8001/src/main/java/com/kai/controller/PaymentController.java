16314fe6af52f2aa4329fb5a439aff002cf8205e
emperwang <544094478@qq.com>
2020-06-15 13:04:47 +0800

    return value add server.port

--- cloud2/cloud-provider-payment8001/src/main/java/com/kai/controller/PaymentController.java
+++ cloud2/cloud-provider-payment8001/src/main/java/com/kai/controller/PaymentController.java
@@ -3,6 +3,8 @@ package com.kai.controller;
 import com.kai.entity.CommonResult;
 import com.kai.entity.Payment;
 import com.kai.service.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,13 +15,16 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult<Payment> getById(@PathVariable(name = "id") Long id){
         Payment paymentById = paymentService.getPaymentById(id);
         if (paymentById != null){
-            return new CommonResult<Payment>(200, "ok", paymentById);
+            return new CommonResult<Payment>(200, "ok, port="+port, paymentById);
         }else{
-            return new CommonResult<Payment>(404, "not found", null);
+            return new CommonResult<Payment>(404, "not found, port="+port, null);
         }
     }
 
@@ -27,9 +32,9 @@ public class PaymentController {
     public Object creatPayment(@RequestBody Payment payment){
         int i = paymentService.insertPayment(payment);
         if (i > 0){
-            return new CommonResult<Payment>(200, "创建成功",null);
+            return new CommonResult<Payment>(200, "创建成功, port="+port,null);
         }else{
-            return new CommonResult<Payment>(500, "创建失败", null);
+            return new CommonResult<Payment>(500, "创建失败, port="+port, null);
         }
     }
 }
