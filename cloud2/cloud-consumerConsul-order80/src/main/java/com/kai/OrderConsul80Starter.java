ed559e4fc1c520d13d7c67b6ba44fb0e815122a7
emperwang <544094478@qq.com>
2020-06-16 11:11:19 +0800

    custom ribbon loadBalance

--- cloud2/cloud-consumerConsul-order80/src/main/java/com/kai/OrderConsul80Starter.java
+++ cloud2/cloud-consumerConsul-order80/src/main/java/com/kai/OrderConsul80Starter.java
@@ -1,11 +1,15 @@
 package com.kai;
 
+import com.rule.RibbonCustomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+// 此处表示: ribbon访问服务cloud-providerconsul-payment时,使用定制的负载均衡算法RibbonCustomRule
+@RibbonClient(name = "cloud-providerconsul-payment", configuration = RibbonCustomRule.class)
 public class OrderConsul80Starter {
     public static void main(String[] args) {
         SpringApplication.run(OrderConsul80Starter.class, args);
