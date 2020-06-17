b96d2974f89ce0617c464901e864de3abe1a7130
emperwang <544094478@qq.com>
2020-06-17 17:57:31 +0800

    add refresh config with cloud-config-client

--- cloud2/cloud-config-client-3355/src/main/java/com/kai/controller/ConfigClientController.java
+++ cloud2/cloud-config-client-3355/src/main/java/com/kai/controller/ConfigClientController.java
@@ -2,11 +2,15 @@ package com.kai.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+// 次注解是打开 自动刷新配置; 不过仍需要人为触发, 发出命令: curl -X POST http://localhost:3355/actuator/refresh
+// 此 actuator/refresh 需要暴露出去, 在配置文件中配置
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
